df1bbd8bfa8bb9750096c4278879b6e35acfa3e8
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-08 10:00:35 -0500

    Refactor to use Lombok

--- src/main/java/com/alaythiaproductions/recipe/services/RecipeServiceImpl.java
+++ src/main/java/com/alaythiaproductions/recipe/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.alaythiaproductions.recipe.services;
 
 import com.alaythiaproductions.recipe.domain.Recipe;
 import com.alaythiaproductions.recipe.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,8 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("In the service");
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
